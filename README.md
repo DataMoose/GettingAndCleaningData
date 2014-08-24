2db8b7eddc33eb7da6b2aec2b18e9badaac4c069
Muhsin <muhsinkarim@outlook.com>
2014-08-24 17:38:53 +1000

    two files

--- README.md
+++ README.md
@@ -1,4 +1,174 @@
-GettingAndCleaningData
-======================
+###Getting and cleaning data course assignment - August 2014
+###Human Activity Recognition Using Smartphones Dataset (Version 1.0)
 
-Getting and Cleaning Data Course Project
+
+The 'run_analysis.R' R programming code organises the 'Human Activity Recognition Using Smartphones Dataset' [1] to prepare tidy data as a space delimited text file 'dataTidy.txt'.
+
+
+
+__Original dataset__
+
+Original dataset may be downloaded at https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+Please refer to the README.txt within the zip file for details of the dataset. 
+
+The 'UCI HAR Dataset' folder in the zip file contains the 'test' and 'train' folder with associated datasets. 
+
+The 'UCI HAR Dataset' folder contains the following files:
+
+- activity_labels.txt: Links the class labels with their activity name.
+
+- features.txt: List of all features.
+
+- features_info.txt: Shows information about the variables used on the feature vector.
+
+- README.txt
+
+The 'test' folder contains the following files:
+
+- 'subject_test.txt': Each row identifies the subject who performed the activity for each window sample for the test dataset.
+
+- 'X_test.txt': Test set.
+
+- 'y_test.txt': Test labels.
+
+The 'train' folder contains the following files:
+
+- 'subject_train.txt': Each row identifies the subject who performed the activity for each window sample for the train dataset. 
+
+- 'X_train.txt': Training set.
+
+- 'y_train.txt': Training labels.
+
+Prior to running 'run_analysis.R' code, the 'UCI HAR Dataset' folder should be stored on a local drive with no modification of the folders and files.
+
+
+
+__Overview of 'run_analysis.R' data transformations__
+
+'run_analysis.R' will perform the following steps:
+
+1. Merges the training and the test sets to create one data set.
+
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
+
+3. Uses descriptive activity names to name the activities in the data set.
+
+4. Appropriately labels the data set with descriptive variable names. 
+
+5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+
+Further details on these sections may be found on the Coursera 'Getting and Cleaning Data' discussion thread 'David's Course Project FAQ' at https://class.coursera.org/getdata-006/forum/thread?thread_id=43
+
+
+
+__1. Merges the training and the test sets to create one data set__
+
+- Location of test data, train data and working directory established.
+
+- 'X_test.txt' and 'X_train.txt' are imported.
+
+- X_train and X_test data is merged.
+
+- dataX data frame is produced, with X_test and X_train merged (with 10,299 rows and 561 columns).
+
+
+
+__2. Extracts only the measurements on the mean and standard deviation for each measurement__
+
+- Out of the 561 columns of measurements, only the measurements pertaining the mean and standard deviation are extracted. 
+
+- The criteria for selecting mean and standard deviation measurements are names those 'features.txt' file names with substring 'mean()' and 'std()'.
+
+- 'features.txt' is imported
+
+- features headers are applied to dataX
+
+- dataX is merged with features as columns names, X_train in the middle and X_text on the bottom.
+
+- Indexing merged data (dataX) with column numbers of measurements with mean or std, dataXf data frame is produced with 66 measurements (with 10,299 rows and 66 columns).
+
+
+		
+__3. Uses descriptive activity names to name the activities in the data set__
+
+- 'subject_test.txt' and 'subject_train.txt' data is imported (contains row identifiers of the subject who performed the activity for test/train set).			
+
+- subject_train and subject_test data is merged.
+
+- 'y_test.txt' and 'y_train.txt' data is imported (activities).
+
+- y_train and y_test data is merged.        
+
+- subject data and y_train/y_test data is merged to the left of dataXf (merged X_test and X_train).		
+
+- Each activity number (y_train, y_test) is subsetted and activity labels are applied as listed in 'activity_labels.txt' (WALKING, WALKING_UPSTAIRS, WALKING_DOWNSTAIRS, SITTING, STANDING, LAYING).
+
+
+		
+__4. Appropriately labels the data set with descriptive variable names__
+
+- Descriptive variable names are applied to data frame dataXfsy.
+
+- Rationale for descriptive variable names: Please refer to 'Codebook.md' for a detailed description of the descriptive variable names. The following summarises the names used:
+
+* 'time': Time domain signal
+
+* 'freq': Frequency domain signal
+
+* 'BodyAcceleration': Body acceleration signals
+
+* 'BodyGyroscope': Body gyroscope (angular velocity) signal	
+
+* 'GravityAcceleration': Gravity acceleration signals
+
+* 'Jerk': Jerk signals
+
+* 'Magnitude': Magnitude of three dimensional signals
+
+* '-X': Axial signal in x direction
+
+* '-Y': Axial signal in y direction
+
+* '-Z': Axial signal in z direction
+
+* 'Mean': Mean estimation of variables
+
+* 'Std': Standard deviation estimation of variables                 
+	
+
+
+__5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject__
+
+- reshape2 library is loaded for melt and cast functions.        
+
+- melt function is used to reshape data to long-format, using subject and variable as id variables.        
+
+- cast function is used to reshape data to wide-format, using subject and activity as id variables.          
+
+- The tidy dataset is written to current working directory as 'dataTidy.txt'. 
+
+- Dimensions of dataTidy are 180 rows and 68 columns. Each row indicates the subject identifier and activity label. Each column describes the signal measurement variables relating to either mean or standard deviation.         
+
+
+
+Location of saved 'dataTidy.txt'         
+
+- 'dataTidy.txt' will be saved in the current working directory ('UCI HAR Dataset' folder).    
+                
+Opening 'dataTidy.txt'              
+
+- To open dataTidy.txt with Excel, delimit with space.
+
+- To upload dataTidy.txt into R, use read.table(yourPathHere) using the path to the location of 'dataTidy.txt'.
+
+
+
+__License:__
+Use of the original dataset in publications must be acknowledged by referencing the following publication: 
+
+[1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
+
+This dataset is distributed AS-IS and no responsibility implied or explicit can be addressed to the authors or their institutions for its use or misuse. Any commercial use is prohibited.
+
+Jorge L. Reyes-Ortiz, Alessandro Ghio, Luca Oneto, Davide Anguita. November 2012.
---
+++ run_analysis.R
@@ -0,0 +1,254 @@
+### Getting and Cleaning Data - Course Project
+
+# run_analysis.R will perform the following steps:
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+# 3. Uses descriptive activity names to name the activities in the data set.
+# 4. Appropriately labels the data set with descriptive variable names. 
+# 5. Creates a second, independent tidy data set with the average of each variable for each 
+# activity and each subject. 
+
+
+#___________________________________________________________________________________________
+
+#### 1. Merges the training and the test sets to create one data set.
+
+
+### Working directory - point to training and test files
+        # User will have 'UCI HAR Dataset' folder with the following directories
+
+        # UCI HAR Dataset
+                # test
+                        # subject_test.txt
+                        # X_test.txt
+                        # y_test.txt
+                # train
+                        # subject_train.txt
+                        # X_train.txt
+                        # y_train.txt
+
+# data folder dir location
+        # User to update directory of 'UCI HAR Dataset' folder location
+        # EXAMPLE If 'UCI HAR Dataset' is in C-drive, dirData is "C:/UCI HAR Dataset/"
+        dirData <- "C:/R/Coursera/Getting and Cleaning Data/Course Project/UCI HAR Dataset/"
+
+# test data dir
+        dirTest <- paste(dirData, "test/", sep="")
+
+# train data dir
+        dirTrain <- paste(dirData, "train/", sep="")
+
+# Set the data dir
+        setwd(dirData)
+
+
+### Import files
+
+# Read in X_test.txt
+        # Test set data from 30% of the volunteers
+        filepath <- paste(dirTest, "X_test.txt", sep="")
+        X_test <- read.table(filepath, stringsAsFactors=FALSE) # no conversion to factors
+                
+# Read in X_train.txt
+        # Train set data from 70% of the volunteers
+        filepath <- paste(dirTrain, "X_train.txt", sep="")
+        X_train <- read.table(filepath, stringsAsFactors=FALSE)  # no conversion to factors        
+
+# Merge X_train with X_test
+        dataX <- rbind(X_train, X_test)
+
+        # dataX data frame is produced, with X_test and X_train merged (dimensions 
+        # [10299 561]).
+
+        
+#___________________________________________________________________________________________
+
+#### 2. Extracts only the measurements on the mean and standard deviation for each 
+#### measurement.
+
+        
+### Criteria for selecting mean and standard deviation measurements       
+        # Out Of the 561 measurements, only measurements with substring 'mean()' and 
+        # 'std()' are selected. 
+        
+        
+# Read in features.txt
+        # List of all features
+        filepath <- paste(dirData, "features.txt", sep="")
+        features <- read.table(filepath, stringsAsFactors=FALSE)
+
+# Transpose features anticlockwise
+        featuresTranspose <- t(features) # features is transposed for merging
+        featuresT <- featuresTranspose[2, ] # subset row with headers only
+                
+# Apply features headers to dataX
+        colnames(dataX) <- featuresT
+
+        # dataX is merged in the following manner:                
+
+                # TOP:    features (as column names)
+                # MIDDLE: X_train
+                # BOTTOM: X_test
+        
+        
+# Get column number of measurement with mean or std        
+        colMean <- grep('mean\\(\\)', features[ ,2]) # col numbers with mean() 
+        colStd <- grep('std\\(\\)', features[ ,2]) # col numbers with std()
+        colKeep <- unique(c(colMean, colStd)) # concatenate column numbers
+        colKeep <- sort(as.numeric(colKeep)) # sorted columns to keep
+        
+# Subset merged data by indexing with columns numbers to keep (with mean and std)
+        dataXf <- dataX[ , colKeep]
+
+        # dataXf data frame is produced with 66 measurements (dimensions [10299 66]).
+        
+
+#___________________________________________________________________________________________
+
+#### 3. Uses descriptive activity names to name the activities in the data set.
+
+
+### Merge subject ID from subject_train.txt
+        
+# Read in subject_test.txt
+        # Row identifiers of the subject who performed the activity for test set
+        filepath <- paste(dirTest, "subject_test.txt", sep="")
+        subject_test <- read.table(filepath, stringsAsFactors=FALSE)
+        
+# Read in subject_train.txt
+        # Row identifiers of the subject who performed the activity for train set
+        filepath <- paste(dirTrain, "subject_train.txt", sep="")
+        subject_train <- read.table(filepath, stringsAsFactors=FALSE)
+        
+# Merge subject_train with subject_test
+        subject <- rbind(subject_train, subject_test)
+        
+        
+### Label column/row names from activity_labels.txt
+
+# Read in y_test.txt (activities)
+        # Activity labels for text set
+        filepath <- paste(dirTest, "y_test.txt", sep="")
+        y_test <- read.table(filepath, stringsAsFactors=FALSE)
+        
+# Read in y_train.txt (activities)        
+        # Activity labels for train set
+        filepath <- paste(dirTrain, "y_train.txt", sep="")
+        y_train <- read.table(filepath, stringsAsFactors=FALSE)        
+
+# Merge y_train with y_test
+        y <- rbind(y_train, y_test)
+
+        
+### Merge subject and y to the left of dataXf (merged X_test and X-train)        
+        dataXfsy <- cbind(cbind(subject, y), dataXf)  
+
+        
+# Include column names for subject and activity 
+        colnames(dataXfsy)[1] <- "subject"
+        colnames(dataXfsy)[2] <- "activity"
+        
+        # dataXfsy is merged in the following manner:                        
+        
+                # "subject"                     "activity"        features
+                # subject_train/subject_test    y_train/y_test    X_train/X_test..........       
+                # subject_train/subject_test    y_train/y_test    X_train/X_test..........
+                # subject_train/subject_test    y_train/y_test    X_train/X_test..........        
+        
+        
+## Subset each activity number (y_train, y_test) and apply labels (activity_labels)
+
+
+# Read in activity_labels.txt       
+        filepath <- paste(dirData, "activity_labels.txt", sep="")
+        activity_labels <- read.table(filepath, stringsAsFactors=FALSE)
+        
+# Apply WALKING actvity label     
+        dataXfsy$activity[dataXfsy$activity == "1"] <- activity_labels[1,2]        
+
+# Apply WALKING_UPSTAIRS actvity label   
+        dataXfsy$activity[dataXfsy$activity == "2"] <- activity_labels[2,2]        
+
+# Apply WALKING_DOWNSTAIRS actvity label        
+        dataXfsy$activity[dataXfsy$activity == "3"] <- activity_labels[3,2]
+                
+# Apply SITTING actvity label        
+        dataXfsy$activity[dataXfsy$activity == "4"] <- activity_labels[4,2]
+        
+# Apply STANDING actvity label        
+        dataXfsy$activity[dataXfsy$activity == "5"] <- activity_labels[5,2]
+        
+# Apply LAYING actvity label        
+        dataXfsy$activity[dataXfsy$activity == "6"] <- activity_labels[6,2]
+        
+        
+#___________________________________________________________________________________________
+
+#### 4. Appropriately labels the data set with descriptive variable names. 
+        
+
+### Rationale for descriptive variable names        
+        
+        # NAME             FEATURE DESCRIPTION
+        # ------------------------------------------------------------
+        # 'time'                Time domain signal
+        # 'freq'                Frequency domain signal
+        # 'BodyAcceleration'    Body acceleration signals
+        # 'BodyGyroscope'       Body gyroscope (angular velocity) signal
+        # 'GravityAcceleration' Gravity acceleration signals
+        # 'Jerk'                Jerk signals
+        # 'Magnitude'           Magnitude of three dimensional signals
+        # '-X'                  Axial signal in x direction
+        # '-Y'                  Axial signal in y direction
+        # '-Z'                  Axial signal in z direction
+        # 'Mean'                Mean estimation of variables
+        # 'Std'                 Standard deviation estimation of variables                 
+        # ____________________________________________________________
+        
+                
+# Steps for descriptive variable name changes
+        colnames(dataXfsy) <- sub("\\(\\)", "", names(dataXfsy)) # remove '()'        
+        colnames(dataXfsy) <- sub("std", "sd", names(dataXfsy)) # hide the 't'
+        colnames(dataXfsy) <- sub("t", "time", names(dataXfsy)) # time
+        colnames(dataXfsy) <- sub("f", "freq", names(dataXfsy)) # freq                
+        colnames(dataXfsy) <- sub("BodyBody", "Body", names(dataXfsy)) # correct 'BobyBody'
+        colnames(dataXfsy) <- sub("Acc", "Acceleration", names(dataXfsy)) # Acceleration
+        colnames(dataXfsy) <- sub("Gyro", "Gyroscope", names(dataXfsy)) # Gyroscope
+        colnames(dataXfsy) <- sub("Mag", "Magnitude", names(dataXfsy)) # Magnitude
+        colnames(dataXfsy) <- sub("mean", "-Mean", names(dataXfsy))    # Mean     
+        colnames(dataXfsy) <- sub("sd", "-Std", names(dataXfsy)) # Std        
+        colnames(dataXfsy) <- sub("-", "", names(dataXfsy)) # remove '-'
+        colnames(dataXfsy)[1] <- "subject" # reapply subject name
+        colnames(dataXfsy)[2] <- "activity" # reapply activity name
+        
+                
+#___________________________________________________________________________________________
+
+#### 5. Creates a second, independent tidy data set with the average of each 
+#### variable for each activity and each subject. 
+
+        
+# Load reshape2 library for melt and cast functions        
+        library(reshape2)        
+
+        # Melt to long-format data, using subject and variable as id variables        
+        dataLong <- melt(dataXfsy, id = c("subject", "activity"))
+
+        # cast to wide-format data, using subject and activity as id variables          
+        dataTidy <- dcast(dataLong, subject + activity ~ variable, mean)
+        
+        
+        # Write the tidy dataset to current working directory
+        write.table(dataTidy, 'dataTidy.txt', row.name=FALSE) # as space delimited text file
+        
+
+# LOCATION OF SAVED 'dataTidy.txt'         
+        # 'dataTidy.txt' will be saved in the current working directory ('UCI HAR Dataset' 
+        # folder)        
+        
+        
+# OPENING 'dataTidy.txt'              
+        # To open dataTidy.txt with Excel, delimit with space         
+
+        # To upload dataTidy.txt into R, use read.table(yourPathHere) using the path to the
+        # location of 'dataTidy.txt'
